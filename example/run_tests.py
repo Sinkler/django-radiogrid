f442c249e14868de0e3dfadb2f0e69df29a3d7ef
Anton Shurashov <sinkler@sinkler.ru>
2015-07-27 11:36:59 +0300

    run tests

--- example/run_tests.py
+++ example/run_tests.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from django.conf import ENVIRONMENT_VARIABLE
+from django.core import management
 from django.core.wsgi import get_wsgi_application
 
 
@@ -13,3 +14,5 @@ else:
     os.environ[ENVIRONMENT_VARIABLE] = sys.argv[1]
 
 application = get_wsgi_application()
+
+management.call_command('test', 'example.app')
